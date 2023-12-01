f165a56feba1a02e50ed6a069d91a9766bce72bd
LopDa093 <dany.lopes.goncalves@gmail.com>
2023-12-01 16:30:16 +0100

    added Basic business mcf

---
+++ routes/Routes/businessRoutes.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/business/info', function () {
+    return view('welcome');
+});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+require __DIR__.'/Routes/businessRoutes.php';
+
+
