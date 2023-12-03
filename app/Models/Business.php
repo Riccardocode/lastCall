416954b884778c186804a14348c6bf0065db0184
Dany Lopes Gonçalves <LopDa093@school.lu>
2023-12-03 14:53:56 +0100

    changed Classes Name + Controller behaviour

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class businessModel extends Model
+class Business extends Model
 {
     use HasFactory;
 }
