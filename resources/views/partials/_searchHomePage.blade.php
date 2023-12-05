eef0699e21e2c6ec9eff9b71de29535602a2ac72
antomart96 <martinanto96@hotmail.fr>
2023-12-05 15:28:09 +0100

    css for homePage and create structure with the css folder

--- resources/views/partials/_searchHomePage.blade.php
+++ resources/views/partials/_searchHomePage.blade.php
@@ -1,5 +1,5 @@
 {{-- Search --}}
-<form action="#">
+<form id="homeSearch" action="#">
     <input type="text" placeholder="Search by location">
     {{-- <button><i>symbole inside the search bar</i></button> --}}
 </form>
