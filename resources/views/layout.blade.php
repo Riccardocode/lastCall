c44d2daf174711bf05dc0f292b7c78f474eec4ab
la-viza <97736137+la-viza@users.noreply.github.com>
2023-12-06 11:51:29 +0100

    + added and styled the footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -83,12 +83,14 @@
         <img src="{{asset('images/logo.png')}}" alt="LastCall logo">
         <nav>
             <ul>
+                <div class="about-us">
                 <li>
                     <a href="/">About us</a>
                 </li>
+                </div>
                 @auth
                 <li>
-                    <span >
+                    <span class="welcome">
                         Welcome {{ auth()->user()->firstname }}
                     </span>
                 </li>
@@ -115,5 +117,41 @@
     </main>
 
     <x-flash-message />
+    
+    <footer>
+        
+        <div class="left-side">
+
+    <ul>
+        <li>
+            <a href=""><i class="fa-brands fa-instagram"></i> &nbsp; <span>Instagram</span></a>
+        </li>
+        <li>
+            <a href=""> <i class="fa-brands fa-facebook"></i>
+               &nbsp; <span>Facebook</span></a>
+        </li>
+        <li>
+            <a href=""><i class="fa-brands fa-linkedin"></i> &nbsp;  <span>LinkedIn</span></a>
+        </li>
+    </ul>
+
+</div>
+<div class="footer">
+<div class="right-side">
+<ul>
+    <li>Our Commitment</li>
+    <li>Contact Us</li>
+    <li>Help and Support</li>
+    <li>lastcall@example.com</li>
+</ul>
+</div>
+
+    </footer>
+    <div class="credentials">
+    <p>LastCall 2023&copy;</p>
+    <p>The website coded by 11111</p>
+</div>
+
+</div>
 </body>
 </html>
