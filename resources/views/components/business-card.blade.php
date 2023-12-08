373c9a6e4b7f0b1c35b6baba6ad5fdd72df80cee
BrandonSteffan <brandon.steffan15@gmail.com>
2023-12-08 15:20:43 +0100

    choosing page logic

--- resources/views/components/business-card.blade.php
+++ resources/views/components/business-card.blade.php
@@ -1,11 +1,14 @@
 @props(['business'])
 <a href="/business/{{ $business->id }}/products">
     <article>
-        <h3>{{ $business->name }} </h3>
-        @if (!empty(session()->get("nearbyBusiness")) && isset(session()->get("nearbyBusiness")[$business->id]))
-            <p>works</p>
-        @endif
-        <div>
+        <div class="bottom">
+            <h3>{{ $business->name }} </h3>
+            @if (!empty(session()->get("nearbyBusiness")) && isset(session()->get("nearbyBusiness")[$business->id]))
+            
+            <p><i class="fa-solid fa-person-walking"></i>  {{ceil(session()->get("nearbyBusiness")[$business->id]["duration"]/60)}} min</p>
+            @endif
+        </div>
+        <div  class="imgBus">
             <img src="/storage/businessImages/restaurantGeneral.png" alt="restaurant">
         </div>
     </article>
