27dab392b028532d775323868d32c641373804ce
antomart96 <martinanto96@hotmail.fr>
2023-12-08 10:17:21 +0100

    all forms design done !

--- resources/views/business/create.blade.php
+++ resources/views/business/create.blade.php
@@ -2,42 +2,27 @@
 
 
 @section('content')
-    <header class="text-center">
-        <h2 class="text-2xl font-bold uppercase mb-1">
-            Register a new Business
-        </h2>
-    </header>
+    <section class="loginSection">
+        <form class="form" action="/business" method="post" enctype="multipart/form-data">
 
-    <form action="/business" method="post" enctype="multipart/form-data">
-
-        @csrf
-        <div class="mb-6">
-            <label for="businessImg" class="inline-block text-lg mb-2">
-                Business Image
-            </label>
-            <input type="file" class="border border-gray-200 rounded p-2 w-full" name="businessImg" />
+            @csrf
+            <h2>
+                Register a new Business
+            </h2>
+            <input type="file" placeholder=" Business Image" name="businessImg" />
             @error('businessImg')
-                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                <p>{{ $message }}</p>
             @enderror
-        </div>
-        <div class="mb-6">
-            <label for="name" class="inline-block text-lg mb-2">Business Name</label>
-            <input type="text" value="{{ old('name') ? old('name') : '' }}"
-                class="border border-gray-200 rounded p-2 w-full" name="name" />
+
+            <input type="text" value="{{ old('name') ? old('name') : '' }}" placeholder="Business Name" name="name" />
             @error('address')
-                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                <p>{{ $message }}</p>
             @enderror
-        </div>
-        <div class="mb-6">
-            <label for="address" class="inline-block text-lg mb-2">Business Address</label>
-            <input type="text" value="{{ old('address') ? old('address') : '' }}"
-                class="border border-gray-200 rounded p-2 w-full" name="address" />
+            <input type="text" value="{{ old('address') ? old('address') : '' }}" placeholder="Business Address"
+                name="address" />
             @error('address')
-                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                <p>{{ $message }}</p>
             @enderror
-        </div>
-        <div class="mb-6">
-            <label for="category_id" class="inline-block text-lg mb-2">Business Category</label>
             <select name="category_id">
                 @foreach ($categories as $category)
                     <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -45,17 +30,16 @@
             </select>
 
             @error('businessCategory')
-                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                <p>{{ $message }}</p>
             @enderror
-        </div>
-
-
-        <div class="mb-6">
-            <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
+            <button class="loginBtn">
                 Create new Business
             </button>
-
-            <a href="/" class="text-black ml-4"> Back </a>
+        </form>
+        <div id="register">
+            <p>
+                <a href="/" class="text-laravel"> Back </a>
+            </p>
         </div>
-    </form>
+    </section>
 @endsection
