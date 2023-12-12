acc2367f691a31ad0bf9585ef217b6a7d39770f5
antomart96 <martinanto96@hotmail.fr>
2023-12-12 09:19:55 +0100

    choosing/sell page done

--- resources/views/users/becomeManager.blade.php
+++ resources/views/users/becomeManager.blade.php
@@ -5,9 +5,11 @@
         <form class="form reveal animationScale" action="/users/{{ $user_id }}/becomeManager" method="POST">
             @csrf
 
-            <h2>Do you want to be a restaurant Manager?</h2>
+            <h2 class="loginSell">Do you want to be a restaurant Manager?</h2>
             @method('PUT')
-            <button class="loginBtn" type="submit">Change Role to Restaurant Manager</button>
+            <div class="loginSell btnContainer">
+                <button class="loginBtn" type="submit">Change Role to Restaurant Manager</button>
+            </div>
         </form>
     </section>
 @endsection
